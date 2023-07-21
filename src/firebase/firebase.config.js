f698021064669c92c595910d084ce98e9e640625
Sourav halder <souravh093@gmail.com>
2023-07-22 01:41:04 +0600

    start my knowledge project and my initial step is done

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCMf5_HEr6hP8k5A_gt3b58-Xx8VqhJf-g",
+  authDomain: "knowledge-door-3245e.firebaseapp.com",
+  projectId: "knowledge-door-3245e",
+  storageBucket: "knowledge-door-3245e.appspot.com",
+  messagingSenderId: "1083776041535",
+  appId: "1:1083776041535:web:f6f56b2dfa4b5d2410ae8a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
